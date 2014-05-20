39813a17f54688624d5e94a06a18ed73f6a21968
Tristan <tristan@rubadeau.com>
2014-05-20 01:55:03 +0000

    halfway through login test

---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "joesmith@example.com"
+    password "password"
+  end
+end
--- spec/features/user_login.rb
+++ spec/features/user_login.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 feature 'Authentication' do
   scenario 'Sign In' do
-    visit root_path
-    click_link "Sign in"
     FactoryGirl.create :user, email: "joesmith@example.com", password: "password"
+    
+    visit sign_in_path
     fill_in "Email", with: "joesmith@example.com"
     fill_in "Password", with: "password"
     click_button "Sign in"
     expect(page).to have_content "Welcome joesmith@example.com"
+    
     click_link "Sign out"
     expect(page).to have_content "Sign in"
   end
