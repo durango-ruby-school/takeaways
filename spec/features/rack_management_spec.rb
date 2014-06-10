d789d2efaf3ebd4717b1e7b3a77e1cf3f6802655
Joe Schrag <joeschrag@gmail.com>
2014-06-10 01:23:27 +0000

    Cleaned up rack management spec

--- spec/features/rack_management_spec.rb
+++ spec/features/rack_management_spec.rb
@@ -5,23 +5,35 @@ feature 'manage racks' do
 
     visit brochure_racks_path
     
+    create_a_rack "Airport"
+    user_sees_flash_message "Rack Successfully Created"
+    expect(page).to have_content "Airport"    
+    
+    update_a_rack "DMR"
+    user_sees_flash_message "Successfully Updated"
+    expect(page).to have_content "DMR"
+    
+    click_link "Delete"
+    expect(page).not_to have_content "DMR"
+  end
+  
+  def create_a_rack rack_name
     click_link "Add Rack"
-    fill_in "Name", with: "Airport"
+    fill_in "Name", with: rack_name
     fill_in "Number of Rows", with: "10"
     fill_in "Number of Columns", with: "4"     
     click_button "Create Rack"
-    expect(page).to have_content "Rack Successfully Created"
-    
-    expect(page).to have_content "Airport"    
+  end
+  
+  def update_a_rack rack_name
     click_link "Edit"
-    fill_in "Name", with: "DMR"
+    fill_in "Name", with: rack_name
     fill_in "Number of Rows", with: "8"
     fill_in "Number of Columns", with: "6"     
     click_button "Update Rack"
-    expect(page).to have_content "Successfully Updated"
-    
-    expect(page).to have_content "DMR"
-    click_link "Delete"
-    expect(page).not_to have_content "DMR"
+  end
+  
+  def user_sees_flash_message message
+    expect(page).to have_css ".flash", text: message
   end
 end
