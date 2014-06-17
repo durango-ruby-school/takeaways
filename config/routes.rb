977a2328eb8c0db086efdec3c263cbbb856b07a3
cj <cjschaff@gmail.com>
2014-06-17 00:51:00 +0000

    Complete client CRUD capabilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
   root 'pages#index'
-  resources :clients, only: [:index, :new, :create, :show]
+  resources :clients, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   
 end
--- spec/features/client_manage_spec.rb
+++ spec/features/client_manage_spec.rb
@@ -7,7 +7,16 @@ feature "Client Management" do
     click_link "New Client"
     fill_in  "Name", :with => "Durango Space"
     click_button "Create"
-    expect(page).to have_content "Successfully"
+    expect(page).to have_content "Successfully Created"
+    
+    click_link "Edit"
+    fill_in "Name", :with => "Animas Code Labs"
+    click_button "Update Client"
+    expect(page).to have_content "Successfully Updated"
+    
+    click_link "Delete"
+    expect(page).to have_content "Deleted"
+    expect(page).to have_content "Clients"
   end
 end
 
