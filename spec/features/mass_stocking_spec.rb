a8ef8330a39214455210a108c79dbae7a31157cd
Joseph Schrag <joeschrag@gmail.com>
2014-10-03 10:25:12 -0600

    Added custom field to make input selector cleaner

--- spec/features/mass_stocking_spec.rb
+++ spec/features/mass_stocking_spec.rb
@@ -14,11 +14,11 @@ feature "Mass Stocking" do
     click_link "Stock Rack"
 
     within object_selector(placement_1) do
-      fill_in "mass_stocking_stockings_attributes_0_quantity", with: 5
+      find("[data-field=quantity]").set(5)
     end
 
     within object_selector(placement_2) do
-      fill_in "mass_stocking_stockings_attributes_1_quantity", with: 10
+      find("[data-field=quantity]").set(10)
     end
 
     click_button "Save"
