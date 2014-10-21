b23d1aec9f9831f0b511237e180b0cb0e8720374
Joseph Schrag <joeschrag@gmail.com>
2014-10-20 19:41:17 -0600

    Changed sender email address to match hosting domain

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,4 @@
 Clearance.configure do |config|
-  config.mailer_sender = 'reply@example.com'
+  config.mailer_sender = "Takeaways App <noreply@#{Rails.application.config.app_domain}>"
   config.allow_sign_up = false
 end
