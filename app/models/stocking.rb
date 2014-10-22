1b9364d8a691150f98683be8838453d14676e591
Jason Sutter <jason@animascodelabs.com>
2014-10-22 13:06:55 -0600

    Fixed issues when quantities are blank on mass stocking page

--- app/models/stocking.rb
+++ app/models/stocking.rb
@@ -5,5 +5,6 @@ class Stocking < ActiveRecord::Base
   validates_presence_of(:placement)
   validates_presence_of(:stocked_on)
   validates_presence_of(:quantity)
+  validates_numericality_of(:quantity, only_integer: true)
   delegate :name, to: :takeaway, prefix: true
 end
