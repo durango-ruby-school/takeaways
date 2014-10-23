b0c59baf6b45b4f28370d6b2f1661836a1f86c71
Jason Sutter <jason@animascodelabs.com>
2014-10-23 08:54:58 -0600

    Added doorkeeper to return a token when signed in

--- app/controllers/api/v1/brochure_racks_controller.rb
+++ app/controllers/api/v1/brochure_racks_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BrochureRacksController < ApplicationController
   skip_before_filter :authorize
-  doorkeeper_for :all
+  doorkeeper_for :all, unless: :signed_in?
 
   def index
     @brochure_racks = BrochureRack.all
