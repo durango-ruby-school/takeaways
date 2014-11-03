bd072225c83f87871a446c50d4735ec3bef144b9
Jason Sutter <jason@animascodelabs.com>
2014-11-03 13:57:51 -0700

    Api for brochure rack details. The placements are also attached.

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :brochure_racks, only: [:index]
+      resources :brochure_racks, only: [:index, :show]
     end
   end
 end
