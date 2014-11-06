70bacc86d138ca07a4feb3f9f5035a7d564b63de
Jason Sutter <jason@animascodelabs.com>
2014-11-06 13:44:47 -0700

    wrote test to not save a generated stocking

--- spec/models/stocking_generator_spec.rb
+++ spec/models/stocking_generator_spec.rb
@@ -17,5 +17,27 @@ describe StockingGenerator do
       expect(generated_stocking.placement_id).to eq(placement_1.id)
       expect(generated_stocking.stocked_on).to eq(date)
     end
+
+    it "does not build a stocking if the quantity is nil" do
+      brochure_rack = create(:brochure_rack)
+      placement_1 = create :placement, brochure_rack: brochure_rack
+      date = Date.yesterday
+      stocking_generator = StockingGenerator.new(brochure_rack, date)
+
+      results = stocking_generator.generate_stockings([{ quantity: nil, placement_id: placement_1.id }])
+
+      expect(results.length).to eq(0)
+    end
+
+    it "does not save the stockings that it generates" do
+      brochure_rack = create(:brochure_rack)
+      placement_1 = create :placement, brochure_rack: brochure_rack
+      date = Date.yesterday
+      stocking_generator = StockingGenerator.new(brochure_rack, date)
+
+      results = stocking_generator.generate_stockings([{ quantity: 5, placement_id: placement_1.id }])
+
+      expect(results.first).not_to be_persisted
+    end
   end
 end
